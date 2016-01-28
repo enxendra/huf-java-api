49e9adec8bf7c30cee913380f6e29837b61d3dd2
Diego Gonzalez <diego.gonzalez@enxendra.com>
2016-01-28 17:14:13 +0100

    Add encoding to header

--- src/main/java/com/enxendra/huf/api/Constants.java
+++ src/main/java/com/enxendra/huf/api/Constants.java
@@ -8,7 +8,7 @@ public class Constants {
     public static final String VERSION = "1.0";
 
     public static final String CONTENT_TYPE = "Content-Type";
-    public static final String APP_JSON = "application/json";
+    public static final String APP_JSON = "application/json; charset=UTF-8";
     public static final String AUTH = "Authorization";
     public static final String BASIC = "Basic";
 }
