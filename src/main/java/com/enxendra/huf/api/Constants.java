e50b417150f45bef98f81bc800e1f2f623680ab9
Diego Gonzalez <diego.gonzalez@enxendra.com>
2016-01-28 15:52:32 +0100

    Add more constants

--- src/main/java/com/enxendra/huf/api/Constants.java
+++ src/main/java/com/enxendra/huf/api/Constants.java
@@ -6,4 +6,9 @@ public class Constants {
     public static final String API_PREM_PROD = "https://premium.hazteunafacturae.com/api/v1";
     public static final String API_STR_TEST = "http://strands.facturacionelectronica.eu/api/v1";
     public static final String VERSION = "1.0";
+
+    public static final String CONTENT_TYPE = "Content-Type";
+    public static final String APP_JSON = "application/json";
+    public static final String AUTH = "Authorization";
+    public static final String BASIC = "Basic";
 }
