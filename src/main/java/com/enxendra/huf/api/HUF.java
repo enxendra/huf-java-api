28a8c1641678daf3d26157ef81a742c48d3c3ac6
Diego Gonzalez <diego.gonzalez@enxendra.com>
2016-01-28 15:52:42 +0100

    Remove unused code in HUF.java

--- src/main/java/com/enxendra/huf/api/HUF.java
+++ src/main/java/com/enxendra/huf/api/HUF.java
@@ -2,8 +2,6 @@ package com.enxendra.huf.api;
 
 public abstract class HUF {
 
-    public static volatile String apiKey;
-    public static volatile String apiVersion;
     private static volatile String apiBase = Constants.API_STR_TEST;
 
     public static String getApiBase() {
