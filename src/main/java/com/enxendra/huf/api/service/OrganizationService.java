fb227f7d6185f0137ce9eef078de0dee3ab62c48
Diego Gonzalez <diego.gonzalez@enxendra.com>
2016-01-28 15:55:10 +0100

    OrganizationService basic scaffold with the get method to retrieve an organization

---
+++ src/main/java/com/enxendra/huf/api/service/OrganizationService.java
@@ -0,0 +1,33 @@
+package com.enxendra.huf.api.service;
+
+import java.util.Map;
+
+import com.enxendra.huf.api.HUF;
+import com.enxendra.huf.api.RequestMethod;
+import com.enxendra.huf.api.RequestOptions;
+import com.enxendra.huf.api.model.OrganizationResponse;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+
+public class OrganizationService extends HUFService {
+
+    private static final String SERVICE_PATH = "organizations";
+
+    public OrganizationService(RequestOptions requestOptions) {
+        this.requestOptions = requestOptions;
+        this.urlBase = HUF.getApiBase() + "/" + SERVICE_PATH;
+    }
+
+    public OrganizationResponse getOrganization(Map<String, Object> params, Long id) throws Exception {
+        JsonObject jsonResponse = callService("/" + id, RequestMethod.GET);
+        OrganizationResponse organizationResponse = (new Gson()).fromJson(jsonResponse, OrganizationResponse.class);
+        return organizationResponse;
+    }
+
+    public OrganizationResponse createOrganization(Map<String, Object> params, RequestOptions requestOptions)
+            throws Exception {
+        JsonObject jsonResponse = callService("/", RequestMethod.GET);
+        OrganizationResponse organizationResponse = (new Gson()).fromJson(jsonResponse, OrganizationResponse.class);
+        return organizationResponse;
+    }
+}
