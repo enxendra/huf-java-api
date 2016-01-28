c73176cbdece4eb1cb547c130d548f6c82f30b5b
Diego Gonzalez <diego.gonzalez@enxendra.com>
2016-01-28 18:33:12 +0100

    Create organizationListResponse

---
+++ src/main/java/com/enxendra/huf/api/model/OrganizationList.java
@@ -0,0 +1,12 @@
+package com.enxendra.huf.api.model;
+
+import java.util.List;
+
+public class OrganizationList {
+
+    private List<Organization> organizations;
+
+    public List<Organization> getOrganizations() {
+        return organizations;
+    }
+}
