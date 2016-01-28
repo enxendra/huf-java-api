775f7e2a9fbb2d49dc132ec51aab5ac2a7ffd1ff
Diego Gonzalez <diego.gonzalez@enxendra.com>
2016-01-28 17:15:29 +0100

    Modify createOrganizationTest to work with new methods

--- src/main/java/com/enxendra/huf/api/test/CreateOrganizationTest.java
+++ src/main/java/com/enxendra/huf/api/test/CreateOrganizationTest.java
@@ -1,11 +1,9 @@
 package com.enxendra.huf.api.test;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import com.enxendra.huf.api.RequestOptions;
 import com.enxendra.huf.api.model.OrganizationResponse;
 import com.enxendra.huf.api.service.OrganizationService;
+import com.google.gson.JsonObject;
 
 public class CreateOrganizationTest extends ServiceTest {
 
@@ -14,25 +12,25 @@ public class CreateOrganizationTest extends ServiceTest {
     public static void main(String[] args) {
         OrganizationService service = new OrganizationService(requestOptions);
 
-        Map<String, Object> params = new HashMap<String, Object>();
+        JsonObject body = new JsonObject();
 
-        params.put("poblation", "A Coruña");
-        params.put("phone", "692977695");
-        params.put("post_code", "15009");
-        params.put("web_url", "www.test.com");
-        params.put("country_code", "ESP");
-        params.put("email", "drom.gom@gmail.com");
-        params.put("address", "Padre Pita, 10");
-        params.put("tax_number", "A85638778S");
-        params.put("name", "Sopo Technologies, S.L.");
-        params.put("province", "A Coruña");
+        body.addProperty("poblation", "A Coruña");
+        body.addProperty("phone", "692977695");
+        body.addProperty("post_code", "15009");
+        body.addProperty("web_url", "www.test.com");
+        body.addProperty("country_code", "ESP");
+        body.addProperty("email", "drom.gom@gmail.com");
+        body.addProperty("address", "Padre Pita, 10");
+        body.addProperty("tax_number", "A85638778S");
+        body.addProperty("name", "Sopo Technologies, S.L.");
+        body.addProperty("province", "A Coruña");
 
         try {
             // Create new organization
-            OrganizationResponse organizationResponse = service.createOrganization(params, requestOptions);
+            OrganizationResponse organizationResponse = service.createOrganization(body, requestOptions);
 
             if (organizationResponse.getData() != null) {
-                System.out.println(organizationResponse.getData().getId());
+                System.out.println("Organización creada con id: " + organizationResponse.getData().getId());
 
             } else {
                 System.out.println("ERROR " + organizationResponse.getErrorCode() + ": "
