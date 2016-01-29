98f842a50d34404bb380dd943e58ca2c6128193f
Diego Gonzalez <diego.gonzalez@enxendra.com>
2016-01-29 09:56:31 +0100

    Add two new methods to OrganizationService

--- src/main/java/com/enxendra/huf/api/service/OrganizationService.java
+++ src/main/java/com/enxendra/huf/api/service/OrganizationService.java
@@ -2,6 +2,7 @@ package com.enxendra.huf.api.service;
 
 import com.enxendra.huf.api.RequestMethod;
 import com.enxendra.huf.api.RequestOptions;
+import com.enxendra.huf.api.model.OrganizationConfigurationResponse;
 import com.enxendra.huf.api.model.OrganizationListResponse;
 import com.enxendra.huf.api.model.OrganizationResponse;
 import com.google.gson.Gson;
@@ -46,6 +47,21 @@ public class OrganizationService extends HUFService {
         OrganizationListResponse organizationListResponse = (new Gson()).fromJson(jsonResponse,
                 OrganizationListResponse.class);
         return organizationListResponse;
+    }
+
+    public OrganizationConfigurationResponse getOrganizationConfiguration(Long id) throws Exception {
+        JsonObject jsonResponse = callService("/" + id + "/configuration", RequestMethod.GET);
+        OrganizationConfigurationResponse organizationConfigurationResponse = (new Gson()).fromJson(jsonResponse,
+                OrganizationConfigurationResponse.class);
+        return organizationConfigurationResponse;
+    }
+
+    public OrganizationConfigurationResponse updateOrganizationConfiguration(Long id, JsonObject body,
+            RequestOptions requestOptions) throws Exception {
+        JsonObject jsonResponse = callService("/" + id + "/configuration", RequestMethod.PUT, body);
+        OrganizationConfigurationResponse organizationConfigurationResponse = (new Gson()).fromJson(jsonResponse,
+                OrganizationConfigurationResponse.class);
+        return organizationConfigurationResponse;
     }
 
 }
