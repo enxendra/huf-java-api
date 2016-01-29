b533cb3216f03db713787963dc851dcffa405af8
Diego Gonzalez <diego.gonzalez@enxendra.com>
2016-01-29 12:36:45 +0100

    Create customerConfigurationResponse

---
+++ src/main/java/com/enxendra/huf/api/model/CustomerConfigurationResponse.java
@@ -0,0 +1,11 @@
+package com.enxendra.huf.api.model;
+
+public class CustomerConfigurationResponse extends HUFResponse {
+
+    private CustomerConfiguration data;
+
+    public CustomerConfiguration getData() {
+        return data;
+    }
+
+}
