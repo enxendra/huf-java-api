13bb82689dfa8a1896da1e59e0e92d3a3620919e
Diego Gonzalez <diego.gonzalez@enxendra.com>
2016-01-29 10:17:11 +0100

    Add response attributes to HUFResponse

--- src/main/java/com/enxendra/huf/api/model/HUFResponse.java
+++ src/main/java/com/enxendra/huf/api/model/HUFResponse.java
@@ -2,6 +2,11 @@ package com.enxendra.huf.api.model;
 
 public class HUFResponse {
 
+    // Response attributes
+    public String responseCode;
+    public String responseMessage;
+
+    // JsonObject attributes
     public String error_code;
     public String error_message;
 
@@ -11,6 +16,22 @@ public class HUFResponse {
 
     public String getErrorMessage() {
         return error_message;
+    }
+
+    public String getResponseCode() {
+        return responseCode;
+    }
+
+    public void setResponseCode(String responseCode) {
+        this.responseCode = responseCode;
+    }
+
+    public String getResponseMessage() {
+        return responseMessage;
+    }
+
+    public void setResponseMessage(String responseMessage) {
+        this.responseMessage = responseMessage;
     }
 
 }
