b793c7e73d3e0dc0a9bbbd5f66aebb566146d101
Diego Gonzalez <diego.gonzalez@enxendra.com>
2016-01-29 12:36:51 +0100

    Create customerList

---
+++ src/main/java/com/enxendra/huf/api/model/CustomerList.java
@@ -0,0 +1,12 @@
+package com.enxendra.huf.api.model;
+
+import java.util.List;
+
+public class CustomerList {
+
+    private List<Customer> customers;
+
+    public List<Customer> getCustomers() {
+        return customers;
+    }
+}
