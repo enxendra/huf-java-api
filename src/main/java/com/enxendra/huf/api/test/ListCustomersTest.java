dae5e3bf134145c328277608e94a7a6c34913625
Diego Gonzalez <diego.gonzalez@enxendra.com>
2016-01-29 12:36:05 +0100

    Create ListCustomersTest

--- src/main/java/com/enxendra/huf/api/test/ListCustomersTest.java
+++ src/main/java/com/enxendra/huf/api/test/ListCustomersTest.java
@@ -5,35 +5,47 @@ import java.util.Map;
 
 import com.enxendra.huf.api.Constants;
 import com.enxendra.huf.api.RequestOptions;
-import com.enxendra.huf.api.service.OrganizationService;
+import com.enxendra.huf.api.model.Customer;
+import com.enxendra.huf.api.model.CustomerListResponse;
+import com.enxendra.huf.api.service.CustomerService;
 
 public class ListCustomersTest extends ServiceTest {
 
     static RequestOptions requestOptions = new RequestOptions(API_KEY, Constants.API_STR_TEST);
 
     public static void main(String[] args) {
-        OrganizationService service = new OrganizationService(requestOptions);
+        CustomerService service = new CustomerService(requestOptions, new Long(363));
 
         Map<String, Object> params = new HashMap<String, Object>();
 
+        params.put("customer_name", "Diego");
+        params.put("customer_tax_number", "A000");
+        params.put("order", "DESC");
+        params.put("page", 1);
+        params.put("page_size", 10);
+
         try {
 
-            // TODO
-            // OrganizationResponse organizationResponse = service.searchOrganization(params, new
-            // Long(359));
-            //
-            // if (organizationResponse.getData() != null) {
-            // System.out.println(organizationResponse.getData().getPoblation());
-            // System.out.println(organizationResponse.getData().getPhone());
-            //
-            // } else {
-            // System.out.println("ERROR " + organizationResponse.getErrorCode() + ": "
-            // + organizationResponse.getErrorMessage());
-            // }
+            CustomerListResponse customerListResponse = service.listCustomers(params);
+
+            if (customerListResponse.getResponseCode().equals(Constants.OK)) {
+                if (customerListResponse.getData() != null && customerListResponse.getData().getCustomers() != null
+                        && customerListResponse.getData().getCustomers().size() > 0) {
+                    for (Customer customer : customerListResponse.getData().getCustomers()) {
+                        System.out.println(customer.getId() + ". " + customer.getName() + " - "
+                                + customer.getTaxNumber());
+                    }
+                } else {
+                    System.out.println("Customers not found");
+                }
+
+            } else {
+                System.out.println("ERROR " + customerListResponse.getErrorCode() + ": "
+                        + customerListResponse.getErrorMessage());
+            }
 
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
-
 }
