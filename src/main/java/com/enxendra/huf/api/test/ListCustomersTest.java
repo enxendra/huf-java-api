d665b9286f8d9dff3796302dfa6f25d5ffeeff78
Diego Gonzalez <diego.gonzalez@enxendra.com>
2016-01-29 09:55:44 +0100

    Implementation of service to get a list of organizations

---
+++ src/main/java/com/enxendra/huf/api/test/ListCustomersTest.java
@@ -0,0 +1,39 @@
+package com.enxendra.huf.api.test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.enxendra.huf.api.Constants;
+import com.enxendra.huf.api.RequestOptions;
+import com.enxendra.huf.api.service.OrganizationService;
+
+public class ListCustomersTest extends ServiceTest {
+
+    static RequestOptions requestOptions = new RequestOptions(API_KEY, Constants.API_STR_TEST);
+
+    public static void main(String[] args) {
+        OrganizationService service = new OrganizationService(requestOptions);
+
+        Map<String, Object> params = new HashMap<String, Object>();
+
+        try {
+
+            // TODO
+            // OrganizationResponse organizationResponse = service.searchOrganization(params, new
+            // Long(359));
+            //
+            // if (organizationResponse.getData() != null) {
+            // System.out.println(organizationResponse.getData().getPoblation());
+            // System.out.println(organizationResponse.getData().getPhone());
+            //
+            // } else {
+            // System.out.println("ERROR " + organizationResponse.getErrorCode() + ": "
+            // + organizationResponse.getErrorMessage());
+            // }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
