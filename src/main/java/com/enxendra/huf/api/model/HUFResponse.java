c9889937a4954c57f4bf2fb16fc5f7bd7a785913
Diego Gonzalez <diego.gonzalez@enxendra.com>
2016-01-29 09:54:37 +0100

    Create HUFResponse to generalize common attributes

---
+++ src/main/java/com/enxendra/huf/api/model/HUFResponse.java
@@ -0,0 +1,16 @@
+package com.enxendra.huf.api.model;
+
+public class HUFResponse {
+
+    public String error_code;
+    public String error_message;
+
+    public String getErrorCode() {
+        return error_code;
+    }
+
+    public String getErrorMessage() {
+        return error_message;
+    }
+
+}
