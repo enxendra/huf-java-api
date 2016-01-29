af02d50badf81b3604bdd9998b0f391f60f8b156
Diego Gonzalez <diego.gonzalez@enxendra.com>
2016-01-29 12:36:58 +0100

    CustomerListResponse

---
+++ src/main/java/com/enxendra/huf/api/model/CustomerListResponse.java
@@ -0,0 +1,10 @@
+package com.enxendra.huf.api.model;
+
+public class CustomerListResponse extends HUFResponse {
+
+    public CustomerList data;
+
+    public CustomerList getData() {
+        return data;
+    }
+}
