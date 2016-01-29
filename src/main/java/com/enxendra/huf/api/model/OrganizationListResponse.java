a4941ef5f34acff3e0bc7270c65b76eb45952c55
Diego Gonzalez <diego.gonzalez@enxendra.com>
2016-01-29 09:56:13 +0100

    Transform responses according to new father

--- src/main/java/com/enxendra/huf/api/model/OrganizationListResponse.java
+++ src/main/java/com/enxendra/huf/api/model/OrganizationListResponse.java
@@ -1,18 +1,8 @@
 package com.enxendra.huf.api.model;
 
-public class OrganizationListResponse {
+public class OrganizationListResponse extends HUFResponse {
 
-    public String error_code;
-    public String error_message;
     public OrganizationList data;
-
-    public String getErrorCode() {
-        return error_code;
-    }
-
-    public String getErrorMessage() {
-        return error_message;
-    }
 
     public OrganizationList getData() {
         return data;
--- src/main/java/com/enxendra/huf/api/model/OrganizationResponse.java
+++ src/main/java/com/enxendra/huf/api/model/OrganizationResponse.java
@@ -1,25 +1,11 @@
 package com.enxendra.huf.api.model;
 
-public class OrganizationResponse {
+public class OrganizationResponse extends HUFResponse {
 
-    public String error_code;
-    public String error_message;
     public Organization data;
-
-    public String getErrorCode() {
-        return error_code;
-    }
-
-    public String getErrorMessage() {
-        return error_message;
-    }
 
     public Organization getData() {
         return data;
-    }
-
-    public void setData(Organization data) {
-        this.data = data;
     }
 
 }
