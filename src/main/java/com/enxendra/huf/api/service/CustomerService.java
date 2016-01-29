e5d20fa21eedd2680a4ca43cf0c4f909e656237a
Diego Gonzalez <diego.gonzalez@enxendra.com>
2016-01-29 12:37:12 +0100

    Implementation of customer service

---
+++ src/main/java/com/enxendra/huf/api/service/CustomerService.java
@@ -0,0 +1,73 @@
+package com.enxendra.huf.api.service;
+
+import java.util.Map;
+
+import com.enxendra.huf.api.RequestMethod;
+import com.enxendra.huf.api.RequestOptions;
+import com.enxendra.huf.api.model.CustomerConfigurationResponse;
+import com.enxendra.huf.api.model.CustomerListResponse;
+import com.enxendra.huf.api.model.CustomerResponse;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+
+public class CustomerService extends HUFService {
+
+    private static final String SERVICE_PATH = "customers";
+
+    public CustomerService(RequestOptions requestOptions, Long orgId) {
+        this.requestOptions = requestOptions;
+        this.urlBase = requestOptions.getApiBase() + "/organizations/" + orgId + "/" + SERVICE_PATH;
+    }
+
+    public CustomerResponse getCustomer(Long customerId) throws Exception {
+        JsonObject jsonResponse = callService("/" + customerId, RequestMethod.GET);
+        CustomerResponse customerResponse = (new Gson()).fromJson(jsonResponse, CustomerResponse.class);
+        return customerResponse;
+    }
+
+    public CustomerResponse createCustomer(JsonObject body, RequestOptions requestOptions) throws Exception {
+        JsonObject jsonResponse = callService("/", RequestMethod.POST, body);
+        CustomerResponse customerResponse = (new Gson()).fromJson(jsonResponse, CustomerResponse.class);
+        return customerResponse;
+    }
+
+    public CustomerResponse updateCustomer(Long id, JsonObject body, RequestOptions requestOptions) throws Exception {
+        JsonObject jsonResponse = callService("/" + id, RequestMethod.PUT, body);
+        CustomerResponse customerResponse = (new Gson()).fromJson(jsonResponse, CustomerResponse.class);
+        return customerResponse;
+    }
+
+    public CustomerResponse deleteCustomer(Long id) throws Exception {
+        JsonObject jsonResponse = callService("/" + id, RequestMethod.DELETE);
+        CustomerResponse customerResponse = (new Gson()).fromJson(jsonResponse, CustomerResponse.class);
+        return customerResponse;
+    }
+
+    public CustomerListResponse listCustomers(Map<String, Object> params) throws Exception {
+        JsonObject jsonResponse = callService("/", RequestMethod.GET, params);
+        CustomerListResponse customerListResponse = (new Gson()).fromJson(jsonResponse, CustomerListResponse.class);
+        return customerListResponse;
+    }
+
+    public CustomerListResponse topCustomers(Map<String, Object> params) throws Exception {
+        JsonObject jsonResponse = callService("/top", RequestMethod.GET, params);
+        CustomerListResponse customerListResponse = (new Gson()).fromJson(jsonResponse, CustomerListResponse.class);
+        return customerListResponse;
+    }
+
+    public CustomerConfigurationResponse getCustomerConfiguration(Long id) throws Exception {
+        JsonObject jsonResponse = callService("/" + id + "/configuration", RequestMethod.GET);
+        CustomerConfigurationResponse customerConfigurationResponse = (new Gson()).fromJson(jsonResponse,
+                CustomerConfigurationResponse.class);
+        return customerConfigurationResponse;
+    }
+
+    public CustomerConfigurationResponse updateCustomerConfiguration(Long id, JsonObject body,
+            RequestOptions requestOptions) throws Exception {
+        JsonObject jsonResponse = callService("/" + id + "/configuration", RequestMethod.PUT, body);
+        CustomerConfigurationResponse customerConfigurationResponse = (new Gson()).fromJson(jsonResponse,
+                CustomerConfigurationResponse.class);
+        return customerConfigurationResponse;
+    }
+
+}
