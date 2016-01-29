63ddd3a7a88a2324cd1bd26c482bc7cb78dade0d
Diego Gonzalez <diego.gonzalez@enxendra.com>
2016-01-29 12:53:04 +0100

    Change visibility to API token

--- src/main/java/com/enxendra/huf/api/test/ServiceTest.java
+++ src/main/java/com/enxendra/huf/api/test/ServiceTest.java
@@ -2,5 +2,5 @@ package com.enxendra.huf.api.test;
 
 public class ServiceTest {
 
-    static String API_KEY = "xxxxx-eb6d-4c93-a12c-99bd8ae43c5b"; // Your API Key here
+    protected static String API_KEY = "xxxx-eb6d-4c93-a12c-99bd8ae43c5b";
 }
