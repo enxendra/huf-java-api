8b69cd57b7e7626708d523c061734ea3afa2927d
Diego Gonzalez <diego.gonzalez@enxendra.com>
2016-02-01 11:52:29 +0100

    DeleteDraftItem

---
+++ src/main/java/com/enxendra/huf/api/test/draft/DeleteDraftItem.java
@@ -0,0 +1,32 @@
+package com.enxendra.huf.api.test.draft;
+
+import com.enxendra.huf.api.Constants;
+import com.enxendra.huf.api.RequestOptions;
+import com.enxendra.huf.api.model.draft.DraftItemResponse;
+import com.enxendra.huf.api.service.DraftService;
+import com.enxendra.huf.api.test.ServiceTest;
+
+public class DeleteDraftItem extends ServiceTest {
+
+    static RequestOptions requestOptions = new RequestOptions(API_KEY, Constants.API_STR_TEST);
+
+    public static void main(String[] args) {
+        DraftService service = new DraftService(requestOptions, new Long(27));
+
+        try {
+            DraftItemResponse draftItemResponse = service.deleteDraftItem(new Long(17028), new Long(1407),
+                    requestOptions);
+
+            if (draftItemResponse.getResponseCode().equals(Constants.OK)) {
+                System.out.println("The item has been deleted");
+
+            } else {
+                System.out.println("ERROR " + draftItemResponse.getErrorCode() + ": "
+                        + draftItemResponse.getErrorMessage());
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
