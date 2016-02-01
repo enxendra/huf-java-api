9ab4e4a3e9ec1cf58c754ed22358284cd72e3f3f
Diego Gonzalez <diego.gonzalez@enxendra.com>
2016-02-01 17:02:07 +0100

    Add new tests

---
+++ src/main/java/com/enxendra/huf/api/test/draft/ValidateDraftTest.java
@@ -0,0 +1,31 @@
+package com.enxendra.huf.api.test.draft;
+
+import com.enxendra.huf.api.Constants;
+import com.enxendra.huf.api.RequestOptions;
+import com.enxendra.huf.api.model.draft.DraftResponse;
+import com.enxendra.huf.api.service.DraftService;
+import com.enxendra.huf.api.test.ServiceTest;
+
+public class ValidateDraftTest extends ServiceTest {
+
+    static RequestOptions requestOptions = new RequestOptions(API_KEY, Constants.API_STR_TEST);
+
+    public static void main(String[] args) {
+        DraftService service = new DraftService(requestOptions, new Long(27));
+
+        try {
+            DraftResponse draftResponse = service.validateDraft(new Long(17029));
+
+            if (draftResponse.getResponseCode().equals(Constants.OK)) {
+                System.out.println(draftResponse.getData().getIsValid());
+                System.out.println(draftResponse.getData().getInvoiceErrors());
+
+            } else {
+                System.out.println("ERROR " + draftResponse.getErrorCode() + ": " + draftResponse.getErrorMessage());
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
