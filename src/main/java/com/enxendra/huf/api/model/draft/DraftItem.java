9c99f765e65d133a1e34c12e90d588ec610833e5
Diego Gonzalez <diego.gonzalez@enxendra.com>
2016-02-01 11:14:50 +0100

    Refactor

--- src/main/java/com/enxendra/huf/api/model/draft/DraftItem.java
+++ src/main/java/com/enxendra/huf/api/model/draft/DraftItem.java
@@ -37,7 +37,7 @@ public class DraftItem {
         return quantity;
     }
 
-    public String getUnit_of_measure() {
+    public String getUnitOfMeasure() {
         return unit_of_measure;
     }
 
@@ -45,11 +45,11 @@ public class DraftItem {
         return price;
     }
 
-    public String getTotal_cost() {
+    public String getTotalCost() {
         return total_cost;
     }
 
-    public String getGross_amount() {
+    public String getGrossAmount() {
         return gross_amount;
     }
 
@@ -57,39 +57,39 @@ public class DraftItem {
         return comments;
     }
 
-    public String getArticle_code() {
+    public String getArticleCode() {
         return article_code;
     }
 
-    public String getDelivery_note_number() {
+    public String getDeliveryNoteNumber() {
         return delivery_note_number;
     }
 
-    public String getDelivery_note_date() {
+    public String getDeliveryNoteSate() {
         return delivery_note_date;
     }
 
-    public String getTransaction_date() {
+    public String getTransactionDate() {
         return transaction_date;
     }
 
-    public String getPeriod_start_date() {
+    public String getPeriodStartDate() {
         return period_start_date;
     }
 
-    public String getPeriod_end_date() {
+    public String getPeriodEndDate() {
         return period_end_date;
     }
 
-    public String getContract_reference() {
+    public String getContractReference() {
         return contract_reference;
     }
 
-    public String getTransaction_reference() {
+    public String getTransactionReference() {
         return transaction_reference;
     }
 
-    public String getFile_reference() {
+    public String getFileReference() {
         return file_reference;
     }
 
@@ -117,7 +117,7 @@ public class DraftItem {
         this.quantity = quantity;
     }
 
-    public void setUnit_of_measure(String unit_of_measure) {
+    public void setUnitOfMeasure(String unit_of_measure) {
         this.unit_of_measure = unit_of_measure;
     }
 
@@ -125,11 +125,11 @@ public class DraftItem {
         this.price = price;
     }
 
-    public void setTotal_cost(String total_cost) {
+    public void setTotalCost(String total_cost) {
         this.total_cost = total_cost;
     }
 
-    public void setGross_amount(String gross_amount) {
+    public void setGrossAmount(String gross_amount) {
         this.gross_amount = gross_amount;
     }
 
@@ -137,39 +137,39 @@ public class DraftItem {
         this.comments = comments;
     }
 
-    public void setArticle_code(String article_code) {
+    public void setArticleCode(String article_code) {
         this.article_code = article_code;
     }
 
-    public void setDelivery_note_number(String delivery_note_number) {
+    public void setDeliveryNoteNumber(String delivery_note_number) {
         this.delivery_note_number = delivery_note_number;
     }
 
-    public void setDelivery_note_date(String delivery_note_date) {
+    public void setDeliveryNoteDate(String delivery_note_date) {
         this.delivery_note_date = delivery_note_date;
     }
 
-    public void setTransaction_date(String transaction_date) {
+    public void setTransactionDate(String transaction_date) {
         this.transaction_date = transaction_date;
     }
 
-    public void setPeriod_start_date(String period_start_date) {
+    public void setPeriodStartDate(String period_start_date) {
         this.period_start_date = period_start_date;
     }
 
-    public void setPeriod_end_date(String period_end_date) {
+    public void setPeriodEndDate(String period_end_date) {
         this.period_end_date = period_end_date;
     }
 
-    public void setContract_reference(String contract_reference) {
+    public void setContractReference(String contract_reference) {
         this.contract_reference = contract_reference;
     }
 
-    public void setTransaction_reference(String transaction_reference) {
+    public void setTransactionReference(String transaction_reference) {
         this.transaction_reference = transaction_reference;
     }
 
-    public void setFile_reference(String file_reference) {
+    public void setFileReference(String file_reference) {
         this.file_reference = file_reference;
     }
 
--- src/main/java/com/enxendra/huf/api/model/draft/Tax.java
+++ src/main/java/com/enxendra/huf/api/model/draft/Tax.java
@@ -7,35 +7,35 @@ public class Tax {
     private String tax_amount;
     private String tax_type;
 
-    public String getTax_base() {
+    public String getTaxBase() {
         return tax_base;
     }
 
-    public String getTax_rate() {
+    public String getTaxRate() {
         return tax_rate;
     }
 
-    public String getTax_amount() {
+    public String getTaxAmount() {
         return tax_amount;
     }
 
-    public String getTax_type() {
+    public String getTaxType() {
         return tax_type;
     }
 
-    public void setTax_base(String tax_base) {
+    public void setTaxBase(String tax_base) {
         this.tax_base = tax_base;
     }
 
-    public void setTax_rate(String tax_rate) {
+    public void setTaxRate(String tax_rate) {
         this.tax_rate = tax_rate;
     }
 
-    public void setTax_amount(String tax_amount) {
+    public void setTaxAmount(String tax_amount) {
         this.tax_amount = tax_amount;
     }
 
-    public void setTax_type(String tax_type) {
+    public void setTaxType(String tax_type) {
         this.tax_type = tax_type;
     }
 
