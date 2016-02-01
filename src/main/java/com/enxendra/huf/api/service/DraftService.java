e9a3c8b68b6ca3eb008c776ea886ef669dc5be1d
Diego Gonzalez <diego.gonzalez@enxendra.com>
2016-02-01 11:52:10 +0100

    Create new services to draftService

--- src/main/java/com/enxendra/huf/api/service/DraftService.java
+++ src/main/java/com/enxendra/huf/api/service/DraftService.java
@@ -4,6 +4,8 @@ import com.enxendra.huf.api.RequestMethod;
 import com.enxendra.huf.api.RequestOptions;
 import com.enxendra.huf.api.model.draft.DraftAttachmentListResponse;
 import com.enxendra.huf.api.model.draft.DraftAttachmentResponse;
+import com.enxendra.huf.api.model.draft.DraftItemListResponse;
+import com.enxendra.huf.api.model.draft.DraftItemResponse;
 import com.enxendra.huf.api.model.draft.DraftListResponse;
 import com.enxendra.huf.api.model.draft.DraftResponse;
 import com.google.gson.Gson;
@@ -80,5 +82,37 @@ public class DraftService extends HUFService {
         DraftAttachmentResponse draftAttachmentResponse = (new Gson()).fromJson(jsonResponse,
                 DraftAttachmentResponse.class);
         return draftAttachmentResponse;
+    }
+
+    public DraftItemResponse createDraftItem(JsonObject body, Long draftId, RequestOptions requestOptions)
+            throws Exception {
+        JsonObject jsonResponse = callService("/" + draftId + "/items", RequestMethod.POST, body);
+        DraftItemResponse draftItemResponse = (new Gson()).fromJson(jsonResponse, DraftItemResponse.class);
+        return draftItemResponse;
+    }
+
+    public DraftItemResponse getDraftItem(Long draftId, Long id) throws Exception {
+        JsonObject jsonResponse = callService("/" + draftId + "/items/" + id, RequestMethod.GET);
+        DraftItemResponse draftItemResponse = (new Gson()).fromJson(jsonResponse, DraftItemResponse.class);
+        return draftItemResponse;
+    }
+
+    public DraftItemListResponse listDraftItems(Long draftId) throws Exception {
+        JsonObject jsonResponse = callService("/" + draftId + "/items", RequestMethod.GET);
+        DraftItemListResponse draftItemListResponse = (new Gson()).fromJson(jsonResponse, DraftItemListResponse.class);
+        return draftItemListResponse;
+    }
+
+    public DraftItemResponse updateDraftItem(JsonObject body, Long draftId, Long id, RequestOptions requestOptions)
+            throws Exception {
+        JsonObject jsonResponse = callService("/" + draftId + "/items/" + id, RequestMethod.PUT, body);
+        DraftItemResponse draftItemResponse = (new Gson()).fromJson(jsonResponse, DraftItemResponse.class);
+        return draftItemResponse;
+    }
+
+    public DraftItemResponse deleteDraftItem(Long draftId, Long id, RequestOptions requestOptions) throws Exception {
+        JsonObject jsonResponse = callService("/" + draftId + "/items/" + id, RequestMethod.DELETE);
+        DraftItemResponse draftItemResponse = (new Gson()).fromJson(jsonResponse, DraftItemResponse.class);
+        return draftItemResponse;
     }
 }
