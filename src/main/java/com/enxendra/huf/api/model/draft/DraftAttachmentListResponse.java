00c963965ae0868aa4ca0689fedbf227b310a9c9
Diego Gonzalez <diego.gonzalez@enxendra.com>
2016-02-01 10:28:06 +0100

    Add new responses

---
+++ src/main/java/com/enxendra/huf/api/model/draft/DraftAttachmentListResponse.java
@@ -0,0 +1,12 @@
+package com.enxendra.huf.api.model.draft;
+
+import com.enxendra.huf.api.model.HUFResponse;
+
+public class DraftAttachmentListResponse extends HUFResponse {
+
+    public DraftAttachmentList data;
+
+    public DraftAttachmentList getData() {
+        return data;
+    }
+}
---
+++ src/main/java/com/enxendra/huf/api/model/draft/DraftAttachmentResponse.java
@@ -0,0 +1,13 @@
+package com.enxendra.huf.api.model.draft;
+
+import com.enxendra.huf.api.model.HUFResponse;
+
+public class DraftAttachmentResponse extends HUFResponse {
+
+    public DraftAttachment data;
+
+    public DraftAttachment getData() {
+        return data;
+    }
+
+}
