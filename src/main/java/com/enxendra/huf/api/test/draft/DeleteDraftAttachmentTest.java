98c41402752610ecfef0b008dcc5b2478805f48a
Diego Gonzalez <diego.gonzalez@enxendra.com>
2016-02-01 10:28:21 +0100

    Get draft attachment

---
+++ src/main/java/com/enxendra/huf/api/test/draft/DeleteDraftAttachmentTest.java
@@ -0,0 +1,32 @@
+package com.enxendra.huf.api.test.draft;
+
+import com.enxendra.huf.api.Constants;
+import com.enxendra.huf.api.RequestOptions;
+import com.enxendra.huf.api.model.draft.DraftAttachmentResponse;
+import com.enxendra.huf.api.service.DraftService;
+import com.enxendra.huf.api.test.ServiceTest;
+
+public class DeleteDraftAttachmentTest extends ServiceTest {
+
+    static RequestOptions requestOptions = new RequestOptions(API_KEY, Constants.API_STR_TEST);
+
+    public static void main(String[] args) {
+        DraftService service = new DraftService(requestOptions, new Long(27));
+
+        try {
+            DraftAttachmentResponse draftAttachmentResponse = service.deleteDraftAttachment(new Long(17028), new Long(
+                    49));
+
+            if (draftAttachmentResponse.getResponseCode().equals(Constants.OK)) {
+                System.out.println("Deleted!");
+            } else {
+                System.out.println("ERROR " + draftAttachmentResponse.getErrorCode() + ": "
+                        + draftAttachmentResponse.getErrorMessage());
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
