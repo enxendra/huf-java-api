f851e796ad7c3524486c05b1485fcae3c7e7ea24
Diego Gonzalez <diego.gonzalez@enxendra.com>
2016-02-01 09:10:14 +0100

    GetDraftTest

---
+++ src/main/java/com/enxendra/huf/api/test/draft/GetDraftTest.java
@@ -0,0 +1,32 @@
+package com.enxendra.huf.api.test.draft;
+
+import com.enxendra.huf.api.Constants;
+import com.enxendra.huf.api.RequestOptions;
+import com.enxendra.huf.api.model.draft.DraftResponse;
+import com.enxendra.huf.api.service.DraftService;
+import com.enxendra.huf.api.test.ServiceTest;
+
+public class GetDraftTest extends ServiceTest {
+
+    static RequestOptions requestOptions = new RequestOptions(API_KEY, Constants.API_STR_TEST);
+
+    public static void main(String[] args) {
+        DraftService service = new DraftService(requestOptions, new Long(27));
+
+        try {
+            DraftResponse draftResponse = service.getDraft(new Long(17028));
+
+            if (draftResponse.getResponseCode().equals(Constants.OK)) {
+                System.out.println(draftResponse.getData().getNumber());
+                System.out.println(draftResponse.getData().getSeries());
+
+            } else {
+                System.out.println("ERROR " + draftResponse.getErrorCode() + ": " + draftResponse.getErrorMessage());
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
