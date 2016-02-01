21e8aa2da5a0597eb8db7b5e61b86ddec24d1e99
Diego Gonzalez <diego.gonzalez@enxendra.com>
2016-02-01 10:27:58 +0100

    Create list for draft attachments

---
+++ src/main/java/com/enxendra/huf/api/model/draft/DraftAttachmentList.java
@@ -0,0 +1,12 @@
+package com.enxendra.huf.api.model.draft;
+
+import java.util.List;
+
+public class DraftAttachmentList {
+
+    private List<DraftAttachment> attachments;
+
+    public List<DraftAttachment> getAttachments() {
+        return attachments;
+    }
+}
