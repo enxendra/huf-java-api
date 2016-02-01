14d871cec22c380136b9ef265fc102d48227e397
Diego Gonzalez <diego.gonzalez@enxendra.com>
2016-02-01 15:02:45 +0100

    Get original invoice test

---
+++ src/main/java/com/enxendra/huf/api/test/invoice/GetOriginalInvoiceTest.java
@@ -0,0 +1,31 @@
+package com.enxendra.huf.api.test.invoice;
+
+import com.enxendra.huf.api.Constants;
+import com.enxendra.huf.api.RequestOptions;
+import com.enxendra.huf.api.model.invoice.InvoiceResponse;
+import com.enxendra.huf.api.service.InvoiceService;
+import com.enxendra.huf.api.test.ServiceTest;
+
+public class GetOriginalInvoiceTest extends ServiceTest {
+
+    static RequestOptions requestOptions = new RequestOptions(API_KEY, Constants.API_STR_TEST);
+
+    public static void main(String[] args) {
+        InvoiceService service = new InvoiceService(requestOptions, new Long(27));
+
+        try {
+            InvoiceResponse invoiceResponse = service.getOriginal(new Long(484));
+
+            if (invoiceResponse.getResponseCode().equals(Constants.OK)) {
+                System.out.println(invoiceResponse.getData().getOriginal());
+
+            } else {
+                System.out
+                        .println("ERROR " + invoiceResponse.getErrorCode() + ": " + invoiceResponse.getErrorMessage());
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
