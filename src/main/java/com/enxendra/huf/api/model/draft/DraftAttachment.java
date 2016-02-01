c42ca9e5f0f38dfef96aa910e1e3aa18becb6943
Diego Gonzalez <diego.gonzalez@enxendra.com>
2016-02-01 10:27:47 +0100

    Create draftAttachment

---
+++ src/main/java/com/enxendra/huf/api/model/draft/DraftAttachment.java
@@ -0,0 +1,26 @@
+package com.enxendra.huf.api.model.draft;
+
+public class DraftAttachment {
+
+    public Long id;
+    public String content;
+    public String name;
+    public String mime;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getMime() {
+        return mime;
+    }
+
+}
