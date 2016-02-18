234985194c175e66161f0ff2c83387aae1d6da43
Diego Gonzalez <diego.gonzalez@enxendra.com>
2016-02-18 16:52:09 +0100

    Create a new HUFException model adapted to our current needs

--- src/main/java/com/enxendra/huf/api/exception/HUFException.java
+++ src/main/java/com/enxendra/huf/api/exception/HUFException.java
@@ -1,28 +1,58 @@
 package com.enxendra.huf.api.exception;
 
 @SuppressWarnings("serial")
-public abstract class HUFException extends Exception {
+public class HUFException extends RuntimeException {
 
-    private String requestId;
-    private Integer code;
+    Throwable e;
+    String message;
+    String code;
 
-    public HUFException(String message, String requestId, Integer code) {
-        super(message, null);
-        this.requestId = requestId;
+    public HUFException(Throwable e) {
+        this.e = e;
+    }
+
+    public HUFException(String message) {
+        this.message = message;
+    }
+
+    public HUFException(Throwable e, String code) {
+        this.e = e;
         this.code = code;
     }
 
-    public HUFException(String message, String requestId, Integer code, Throwable e) {
-        super(message, e);
+    public HUFException(String message, String code, Throwable e) {
+        this.e = e;
+        this.message = message;
         this.code = code;
-        this.requestId = requestId;
     }
 
-    public String getRequestId() {
-        return requestId;
+    public HUFException(String code, String message) {
+        this.message = message;
+        this.code = code;
     }
 
-    public Integer getCode() {
+    public Throwable getEx() {
+        return e;
+    }
+
+    public void setE(Throwable e) {
+        this.e = e;
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getCode() {
         return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
     }
 }
