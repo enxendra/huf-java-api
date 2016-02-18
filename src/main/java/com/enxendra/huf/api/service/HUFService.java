f7c247b46bd4ae5e5a2bf211e89c8e0dc58444e5
Diego Gonzalez <diego.gonzalez@enxendra.com>
2016-02-18 16:51:53 +0100

    Changes in HUFService to work with HUFExceptions

--- src/main/java/com/enxendra/huf/api/service/HUFService.java
+++ src/main/java/com/enxendra/huf/api/service/HUFService.java
@@ -12,6 +12,7 @@ import java.util.Map;
 import com.enxendra.huf.api.Constants;
 import com.enxendra.huf.api.RequestMethod;
 import com.enxendra.huf.api.RequestOptions;
+import com.enxendra.huf.api.exception.HUFException;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
@@ -38,79 +39,82 @@ public class HUFService {
         this.urlBase = urlBase;
     }
 
-    protected JsonObject callService(String path, RequestMethod requestMethod) throws Exception {
+    protected JsonObject callService(String path, RequestMethod requestMethod) throws HUFException {
         return callService(path, requestMethod, null, null);
     }
 
     protected JsonObject callService(String path, RequestMethod requestMethod, Map<String, Object> queryParams)
-            throws Exception {
+            throws HUFException {
         return callService(path, requestMethod, queryParams, null);
     }
 
-    protected JsonObject callService(String path, RequestMethod requestMethod, JsonObject body) throws Exception {
+    protected JsonObject callService(String path, RequestMethod requestMethod, JsonObject body) throws HUFException {
         return callService(path, requestMethod, null, body);
     }
 
     protected JsonObject callService(String path, RequestMethod requestMethod, Map<String, Object> queryParams,
-            JsonObject body) throws Exception {
+            JsonObject body) throws HUFException {
 
         JsonReader reader;
         JsonParser parser = new JsonParser();
         String endpoint = this.urlBase + path;
 
-        // If there are queryParams and it is a GET request
-        if (queryParams != null && queryParams.size() > 0 && requestMethod.equals(RequestMethod.GET))
-            endpoint = this.urlBase + path + getQuery(queryParams);
-
-        System.out.println(requestMethod.name() + " request to: " + endpoint);
-        HttpURLConnection urlConnection = (HttpURLConnection) new URL(endpoint).openConnection();
-
-        // Add header
-        System.out.println(Constants.CLIENT_VERSION + ": " + requestOptions.getClientVersion());
-
-        if (requestOptions.getUserToken() != null)
-            System.out.println(Constants.USER_TOKEN + ": " + requestOptions.getUserToken());
-
-        System.out.println(Constants.CONTENT_TYPE + ": " + Constants.APP_JSON);
-        System.out.println(Constants.AUTH + ": " + Constants.BASIC + " " + requestOptions.getApiKey());
-
-        urlConnection.setRequestProperty(Constants.CONTENT_TYPE, Constants.APP_JSON);
-
-        if (requestOptions.getUserToken() != null)
-            urlConnection.setRequestProperty(Constants.USER_TOKEN, requestOptions.getUserToken());
-
-        urlConnection.setRequestProperty(Constants.CLIENT_VERSION, requestOptions.getClientVersion());
-        urlConnection.setRequestProperty(Constants.AUTH, Constants.BASIC + " " + requestOptions.getApiKey());
-        urlConnection.setRequestMethod(requestMethod.name());
-
-        // Add body (only if there is a body JsonObject and it is not a GET request)
-        if (body != null && !requestMethod.equals(RequestMethod.GET)) {
-            urlConnection.setDoOutput(true);
-            System.out.println("Body: " + body.toString());
-            OutputStream os = urlConnection.getOutputStream();
-            os.write(body.toString().getBytes());
-            os.flush();
-        }
-
-        urlConnection.connect();
-
-        System.out.println("Response code: " + urlConnection.getResponseCode());
-        System.out.println("Response message: " + urlConnection.getResponseMessage());
-
         try {
-            reader = new JsonReader(new InputStreamReader(urlConnection.getInputStream()));
-        } catch (IOException e) {
-            reader = new JsonReader(new InputStreamReader(urlConnection.getErrorStream()));
+
+            // If there are queryParams and it is a GET request
+            if (queryParams != null && queryParams.size() > 0 && requestMethod.equals(RequestMethod.GET))
+                endpoint = this.urlBase + path + getQuery(queryParams);
+
+            System.out.println(requestMethod.name() + " request to: " + endpoint);
+            HttpURLConnection urlConnection = (HttpURLConnection) new URL(endpoint).openConnection();
+
+            // Add header
+            System.out.println(Constants.CLIENT_VERSION + ": " + requestOptions.getClientVersion());
+
+            if (requestOptions.getUserToken() != null)
+                System.out.println(Constants.USER_TOKEN + ": " + requestOptions.getUserToken());
+
+            System.out.println(Constants.CONTENT_TYPE + ": " + Constants.APP_JSON);
+            System.out.println(Constants.AUTH + ": " + Constants.BASIC + " " + requestOptions.getApiKey());
+
+            urlConnection.setRequestProperty(Constants.CONTENT_TYPE, Constants.APP_JSON);
+
+            if (requestOptions.getUserToken() != null)
+                urlConnection.setRequestProperty(Constants.USER_TOKEN, requestOptions.getUserToken());
+
+            urlConnection.setRequestProperty(Constants.CLIENT_VERSION, requestOptions.getClientVersion());
+            urlConnection.setRequestProperty(Constants.AUTH, Constants.BASIC + " " + requestOptions.getApiKey());
+            urlConnection.setRequestMethod(requestMethod.name());
+
+            // Add body (only if there is a body JsonObject and it is not a GET request)
+            if (body != null && !requestMethod.equals(RequestMethod.GET)) {
+                urlConnection.setDoOutput(true);
+                System.out.println("Body: " + body.toString());
+                OutputStream os = urlConnection.getOutputStream();
+                os.write(body.toString().getBytes());
+                os.flush();
+            }
+
+            urlConnection.connect();
+
+            try {
+                reader = new JsonReader(new InputStreamReader(urlConnection.getInputStream()));
+            } catch (IOException e) {
+                reader = new JsonReader(new InputStreamReader(urlConnection.getErrorStream()));
+            }
+
+            JsonElement rootElement = parser.parse(reader);
+
+            // Add response code and message to the json object
+            JsonObject jsonObject = rootElement.getAsJsonObject();
+            jsonObject.addProperty("responseCode", urlConnection.getResponseCode());
+            jsonObject.addProperty("responseMessage", urlConnection.getResponseMessage());
+
+            return jsonObject;
+
+        } catch (Exception e) {
+            throw new HUFException(e.getMessage(), "S/N", e);
         }
-
-        JsonElement rootElement = parser.parse(reader);
-
-        // Add response code and message to the json object
-        JsonObject jsonObject = rootElement.getAsJsonObject();
-        jsonObject.addProperty("responseCode", urlConnection.getResponseCode());
-        jsonObject.addProperty("responseMessage", urlConnection.getResponseMessage());
-
-        return jsonObject;
 
     }
 
@@ -133,4 +137,5 @@ public class HUFService {
         return "?" + result.toString();
 
     }
+
 }
