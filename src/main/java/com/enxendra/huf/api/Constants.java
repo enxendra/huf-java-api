3f23908e38c0347b1eefd8e1c886df67b93320a6
Diego Gonzalez <diego.gonzalez@enxendra.com>
2016-04-13 07:57:19 -0400

    Change in constants

--- src/main/java/com/enxendra/huf/api/Constants.java
+++ src/main/java/com/enxendra/huf/api/Constants.java
@@ -4,7 +4,7 @@ public class Constants {
 
     public static final String API_PREM_TEST = "https://demo.hazteunafacturae.com/api/v1";
     public static final String API_PREM_PROD = "https://premium.hazteunafacturae.com/api/v1";
-    public static final String API_STR_TEST = "http://strands.facturacionelectronica.eu/api/v1"; // "http://localhost:8080/NewHUFPremium/api/v1";
+    public static final String API_STR_TEST = "http://strands.facturacionelectronica.eu/api/v1";
     public static final String VERSION = "1.0";
 
     public static final String CONTENT_TYPE = "Content-Type";
@@ -16,4 +16,5 @@ public class Constants {
 
     public static final String OK = "200";
     public static final String CREATED_OR_UPDATED = "201";
+
 }
